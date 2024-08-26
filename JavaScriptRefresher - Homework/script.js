6b46e4e7636b751356eaa53f02d61e8f77064af3
Tameikat92 <148349545+Tameikat92@users.noreply.github.com>
2024-08-25 21:33:56 -0400

    Add files via upload

---
+++ JavaScriptRefresher - Homework/script.js
@@ -0,0 +1,30 @@
+document.write("JavaScript Refresher")
+
+let firstName = "Tameika"
+let lastName = "Thompson"
+let fullName = firstName+" "+lastName;
+console.log(fullName)
+
+let population = 631_524;
+console.log(population)
+console.log(population * 3)
+
+javaScriptIsCool = true;
+
+let colors = ["red","blue","yellow","green","purple"];
+
+let num = 1;
+
+while (num<=5) {
+   console.log(num);
+   num = num +1;
+
+}
+
+let number = 0;
+
+while (number <=4) {
+    console.log(number)
+    number += 1;
+}
+
